33ef24753e22844e8358f1844dde4f6e0df28a15
nikolainesterov <nikolainesterov2002@gmail.com>
2026-05-26 13:59:46 -0600

    Added peaks + prairies event and fixed button in bio page to "Make a Booking"

--- js/calendar.js
+++ js/calendar.js
@@ -24,6 +24,17 @@ var EVENTS = [
     upcoming: true                  // set false once event has passed
   },
 
+   {
+    title:   "Peaks + Prairies Ensemble: <br> 5th Anniversary Concert",
+    date:    '2026-06-28',         // YYYY-MM-DD format
+    time:    '2:00 PM',
+    venue:   "St. George's in the Pines Anglican Church",
+    city:    'Banff, Alberta',
+    image:   'https://lh3.googleusercontent.com/sitesv/AA5AbUC0thcpgDxYQRakrXpA4HP5l0kX9izI3BhOEY-HEErgckJSSEPcySokfOq56ObSQq1f-oxRGyi74ctc968Tq5jd0Mmuz_KKMuWrg5YGJAe6epQZAO9oPRBYfYjlUbpPopU64CsfYZ_oSpMaIph3usevdO_UYbYhmirSpB5NX9uSNI1oTX_N6N67=w1280',
+    link:    'https://www.livefromtherockies.ca/events/peaks-prairies-ensemble-5th-anniversary-concert',
+    upcoming: true                  // set false once event has passed
+  },
+
   /* ── ADD MORE EVENTS BELOW ──
   {
     title:   'Evening Recital',
